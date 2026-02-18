25f823c8708bf937eb0d8b045bdd8085322f84c1
tkhs-0114 <tkhs.mail0114@gmail.com>
2026-02-18 22:52:47 +0900

    dev: Goから音声ファイルを送信し、React側で再生

--- ReactNative/app/(tabs)/home.tsx
+++ ReactNative/app/(tabs)/home.tsx
@@ -1,38 +1,43 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useState } from "react";
 import ScreenTitle from "@/components/ScreenTitle";
 import Button from "@/components/ui/Button";
 import TimeBox from "@/components/ui/TimeBox";
 import TimePickerModal from "@/components/ui/TimeBoxEdit";
 import { MAINCOLORS } from "@/constants/colors";
+import { useAlarm } from "@/hooks/useAlarm";
+import { setData } from "@/utils/alarm";
 
 export default function Home() {
-    const [time, setTime] = useState("09:00");
-    const [isEditing, setIsEditing] = useState(false);
-    return (
-        <View
-        style={{
-            flex: 1,
-            backgroundColor: MAINCOLORS.Background,
-            padding: 16,
-        }}
-        >
-        <ScreenTitle title="アラーム時間" />
-        {/* 編集モードじゃない時はTimeBox、編集時はピッカーを表示 */}
-        {!isEditing ? (
-            <TimeBox time={time} onPress={() => setIsEditing(true)} />
-        ) : (
-            <View style={{ flex: 1, justifyContent: "center" }}>
-                <TimePickerModal 
-                    initialTime={time} 
-                    onConfirm={(t) => {
-                        setTime(t);
-                        setIsEditing(false);
-                    }} 
-                />
-            </View>
-        )}
-        <Button text="おきた"/>
+  const [time, setTime] = useState("09:00");
+  const [isEditing, setIsEditing] = useState(false);
+  const { setVolume } = useAlarm();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: MAINCOLORS.Background,
+        padding: 16,
+      }}
+    >
+      <ScreenTitle title="アラーム時間" />
+      {/* 編集モードじゃない時はTimeBox、編集時はピッカーを表示 */}
+      {!isEditing ? (
+        <TimeBox time={time} onPress={() => setIsEditing(true)} />
+      ) : (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <TimePickerModal
+            initialTime={time}
+            onConfirm={(t) => {
+              setTime(t);
+              setIsEditing(false);
+            }}
+          />
         </View>
-    );
+      )}
+      <Button text="アラーム取得" onPress={setData} />
+      <Button text="音声再生" onPress={() => setVolume(50)} />
+    </View>
+  );
 }
