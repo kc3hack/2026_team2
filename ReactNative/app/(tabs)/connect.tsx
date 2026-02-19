8225761eea34ac89808e0b9a7e9f7b4ed6597b0c
tkhs-0114 <tkhs.mail0114@gmail.com>
2026-02-19 19:42:16 +0900

    レベルを使用

--- ReactNative/app/(tabs)/connect.tsx
+++ ReactNative/app/(tabs)/connect.tsx
@@ -1,19 +1,21 @@
 import { View, Text } from "react-native";
 import ScreenTitle from "@/components/ScreenTitle";
 import { MAINCOLORS } from "@/constants/colors";
+import { useAudioLevel } from "@/hooks/useAudioLevel";
 
-export default function Connect() { 
-    return (
-        <View
-        style={{
-            flex: 1,
-            backgroundColor: MAINCOLORS.Background,
-            padding: 16,
-        }}
-        >
-        <ScreenTitle title="iot接続" />
-        <Text>aaaa</Text>
-        
-        </View>
-    )
+export default function Connect() {
+  const volume = useAudioLevel();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: MAINCOLORS.Background,
+        padding: 16,
+      }}
+    >
+      <ScreenTitle title="iot接続" />
+      <Text>音量: {volume}</Text>
+    </View>
+  );
 }
